dbfd19225edde2ab582e35002a98be104dad3e90
poncho_boncho <Sorkolas@mail.ru>
2023-04-11 00:22:03 +0300

    added snackbar and state logic

--- app/src/main/java/poncho/boncho/lemonade/Lemonade.kt
+++ app/src/main/java/poncho/boncho/lemonade/Lemonade.kt
@@ -1,15 +1,42 @@
 package poncho.boncho.lemonade
 
 import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
+import org.w3c.dom.Text
 
 class Lemonade {
 
-    var condition = 1
+    var state = "SELECT"
+    var lemonSize: Int = 10
+    var squeezeCount: Int = 0
 
-    fun nextCondition(){
-        if (condition>=3){
-            condition=1
+    public fun stateIsRestart() {
+        state = "SELECT"
+
+    }
+
+    public fun stateIsDrink() {
+        state = "RESTART"
+        lemonSize = -1
+    }
+
+    public fun stateIsSqueeze(defaultSnackbar: Snackbar) {
+        squeezeCount+=1
+        println(squeezeCount)
+        defaultSnackbar.setText("Вы выжали лемон ${squeezeCount} раз")
+        defaultSnackbar.show()
+        if (lemonSize !=0)
+            lemonSize--
+        else {
+            state = "DRINK"
+            squeezeCount=0
         }
-        else condition++
+    }
+
+    public fun stateIsSelect() {
+        state = "SQUEEZE"
+        lemonSize = 10
+        squeezeCount = 0
     }
 }
--- app/src/main/java/poncho/boncho/lemonade/MainActivity.kt
+++ app/src/main/java/poncho/boncho/lemonade/MainActivity.kt
@@ -1,29 +1,59 @@
 package poncho.boncho.lemonade
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.snackbar.SnackbarContentLayout
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val lemonImage: ImageView = findViewById(R.id.imageView)
         val lemonade = Lemonade()
-        lemonImage.setImageResource(R.drawable.lemonthree_0001)
-        var condition = 1
-        lemonImage.setOnClickListener{ clickLemonImage(lemonImage, lemonade) }
+        var ccmain: ConstraintLayout = findViewById(R.id.layoutM)
+        val defaultSnackbar = Snackbar.make(ccmain, "Вы выжали лемон ${lemonade.squeezeCount} раз",Snackbar.LENGTH_LONG)
+        val lemonImage: ImageView = findViewById(R.id.imageView)
+        lemonImage.setImageResource(R.drawable.lemon_tree)
+        lemonImage.setOnClickListener{clickLemonImage(lemonade,defaultSnackbar)}
     }
 
-    private fun clickLemonImage(lemonImage: ImageView, lemonade: Lemonade){
-        lemonade.nextCondition()
-        val drawableResours = when (lemonade.condition){
-            1 -> R.drawable.lemonthree_0001
-            2 -> R.drawable.lemonstep2
-            else -> R.drawable.lemonstep2
+    private fun clickLemonImage(lemonade: Lemonade,defaltSnackbar: Snackbar){
+        when (lemonade.state){
+            "SELECT"  -> lemonade.stateIsSelect()
+            "SQUEEZE" -> lemonade.stateIsSqueeze(defaltSnackbar)
+            "DRINK"   -> lemonade.stateIsDrink()
+           else -> lemonade.stateIsRestart()
+       }
+        setViewElements(lemonade.state)
+    }
+
+    private fun setViewElements(lemonadeState: String) {
+
+        val lemonImage: ImageView = findViewById(R.id.imageView)
+        val instruction: TextView = findViewById(R.id.textView)
+
+        val drawableResourse = when (lemonadeState){
+            "SELECT"  -> R.drawable.lemon_tree
+            "SQUEEZE" -> R.drawable.lemon_squeeze
+            "DRINK"   -> R.drawable.lemon_drink
+            else -> R.drawable.lemon_restart
         }
-        lemonImage.setImageResource(drawableResours)
+        val textResourse = when (lemonadeState){
+            "SELECT"  -> "Нажмите, чтобы выбрать лимон!"
+            "SQUEEZE" -> "Нажмите, чтобы выжать сок из лимона!"
+            "DRINK"   -> "Нажмите, чтобы выпить свой лимонад!"
+            else -> "Нажмите, чтобы начать снова!"
+        }
+
+        lemonImage.setImageResource(drawableResourse)
+        instruction.setText(textResourse)
 
     }
+
 }
